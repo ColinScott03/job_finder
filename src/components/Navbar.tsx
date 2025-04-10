f0178d271784600241d3a206271b6cf813fceddd
colinscott0 <colinscott.cas@gmail.com>
2025-04-09 22:46:21 -0400

    Add logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
-import "./navbar.css"
+import "./Navbar.css"
+import Image from "next/image"
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-
+            <Image
+                src="/logo.png"
+                alt="Tender Logo"
+                width={150}
+                height={0}
+            />
         </nav>
     );
 }
