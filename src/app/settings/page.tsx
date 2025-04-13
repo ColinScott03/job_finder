5eba8b9541a13dec723eec277fe85be2e92b9d26
Roman Edwards <rne52318@uga.edu>
2025-04-12 21:31:41 -0400

    Created a settings page at src/app/settings. Also created Content.tsx component for a baseline w/ background similar to the one made in Figma.

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import Content from "../../components/Content";
+import { useState } from 'react';
+
+const Settings = () => {
+    const initialFormState = {
+        owner: 0,
+        name: '',
+        interests: '',
+        location: '',
+        industry: '',
+        fullTime: false,
+        partTime: false,
+        inPerson: false,
+        remote: false
+    };
+    
+    const [formData, setFormData] = useState(initialFormState);
+    
+    const clearForm = () => {
+        setFormData(initialFormState);
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(JSON.stringify(formData));
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const { name, value, type, checked } = e.target;
+        setFormData(prev => ({
+        ...prev,
+        [name]: type === 'checkbox' ? checked : (name === 'owner' ? Number(value) : value),
+    }));
+    }
+
+    return (
+        <Content>
+            <div className="flex flex-col items-center m-20 w-[40vw] h-210 bg-[#20262B] border-10">
+                <h2 className="m-5 text-3xl text-neutral-300 block">Account Details</h2>
+                <form onSubmit={handleSubmit} className="space-y-4 w-[25vw] text-neutral-300 justify-end">
+                    <h3 className="text-xl">Name</h3>
+                    <input
+                    name="name"
+                    type="string"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Name"
+                    required
+                    className="w-full p-2 border bg-slate-900 border-gray-300 rounded"
+                    />
+                    <h3 className="text-xl">Interests</h3>
+                    <input
+                    name="interests"
+                    type="string"
+                    value={formData.interests}
+                    onChange={handleChange}
+                    placeholder="E.g. Gardening, Social, Computers"
+                    required
+                    className="w-full p-2 border bg-slate-900 border-gray-300 rounded"
+                    />
+                    <h3 className="text-xl">Location</h3>
+                    <input
+                    name="location"
+                    type="string"
+                    value={formData.location}
+                    onChange={handleChange}
+                    placeholder="City, State"
+                    required
+                    className="w-full p-2 border bg-slate-900 border-gray-300 rounded"
+                    />
+
+                    <h3 className="text-xl">Industry</h3>
+                    <select
+                    name="industry"
+                    //type="string"
+                    value={formData.industry}
+                    onChange={handleChange}
+                    required
+                    className="w-full p-2 border bg-slate-900 border-gray-300 rounded"
+                    >
+                        <option value='' disabled selected>Select</option>
+                        <option value='it'>I.T.</option>
+                        <option value='research'>Research</option>
+                        <option value='sales'>Sales</option>
+                        <option value='engineering'>Engineering</option>
+                    </select>
+
+                    <h3 className="text-xl">Job Type</h3>
+                    <div className="flex flex-col justify-between items-center w-full h-50 p-2 border bg-slate-900 border-gray-300 rounded">
+                        <div className="">
+                            <input type="checkbox" name="fullTime" checked={formData.fullTime} onChange={handleChange} id="fullTimeBox" className="h-4 w-4"/>
+                            <label className="text-2xl">Full-time</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="partTime" checked={formData.partTime} onChange={handleChange} id="partTimeBox" className="h-4 w-4"/>
+                            <label className="text-2xl">Part-time</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="inPerson" checked={formData.inPerson} onChange={handleChange} id="inPersonBox" className="h-4 w-4"/>
+                            <label className="text-2xl">In Person</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="remote" checked={formData.remote} onChange={handleChange} id="remoteBox" className="h-4 w-4"/>
+                            <label className="text-2xl">Remote</label>
+                        </div>
+                    </div>
+                    <div className="flex justify-center gap-20">
+                        <button type="submit" className="bg-slate-900 text-white px-4 py-2 rounded hover:bg-red-800 mt-4">Submit</button>
+                        <button onClick={clearForm}className="bg-slate-900 text-white px-4 py-2 rounded hover:bg-red-800 mt-4">Clear</button>
+                    </div>
+                </form>
+            </div>
+        </Content>
+    )
+};
+export default Settings;
---
+++ src/components/Content.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+interface ContentProps {
+    children: React.ReactNode;
+    className?: string;
+  }
+  
+  const Content = ({ children, className = "" }: ContentProps) => {
+    return (
+        <div className="bg-red-500 min-h-screen border-4 border-red-900 flex justify-center items-center">
+          
+        {children}
+      </div>
+    );
+  };
+
+export default Content;
